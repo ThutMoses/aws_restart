90a2f45822a31a8aef0460c9d34f0c955c7ceb84
Moses <73477488+mmpofu87@users.noreply.github.com>
2021-01-06 15:41:36 +0000

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+#if i have 5 or more bananas
+bananas = 6
+if bananas >= 5:
+    print("my bunch is large")
+elif bananas >=1 and bananas <=4: print("my bunch is small")
+else:
+    print("I don't have any bananas")
+    
