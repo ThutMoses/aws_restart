6f5638bc0c1befbfefb0a65d7c21ba50de894ded
Moses <73477488+mmpofu87@users.noreply.github.com>
2021-01-07 09:42:46 +0000

    Add files via upload

---
+++ fun maths.py
@@ -0,0 +1,7 @@
+# get two numbers from user and display the total of all the numbers between them
+
+num1=int(input("enter the first number"))
+num2=int(input("Enter second number"))
+
+num1=num1+num2
+print (num1)
